a05e3e10dabf01b3e442bfdff2aed1582bc36b49
ritesh_gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-06 21:04:51 +0530

    Add files via upload

---
+++ program99X.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int iCnt = 0 ;
+    int Arr[5]= {10,20,30,40,50,};
+
+    for(iCnt =4 ;iCnt >= 0 ; iCnt--)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+    return 0 ;
+
+}
