0e89521b08ac98558188cacf585531d1f3b23b9a
ritesh_gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-06 21:05:15 +0530

    Add files via upload

---
+++ program100.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int iCnt = 0 ;
+    int Arr[5]= {0};
+
+    printf("Enter the Elements:\n");
+    
+    scanf("%d",&Arr[0]);
+    scanf("%d",&Arr[1]);
+    scanf("%d",&Arr[2]);
+    scanf("%d",&Arr[3]);
+    scanf("%d",&Arr[4]);
+
+     printf("Elements of Array are :\n");
+     for(iCnt = 0 ;iCnt < 5; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+
+
+    return 0 ;
+
+}
