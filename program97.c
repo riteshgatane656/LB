8a8c06f3f973d1fb3b8b2b27fff9c0508ba432e3
ritesh_gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-06 21:02:35 +0530

    Add files via upload

---
+++ program97.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+    int Arr[7] ;          //Memory allocated in data(GLOBAL Variable)
+
+
+int main()
+{
+
+    Arr[0]=10;
+    Arr[3]=20;
+    Arr[6]=30;
+    printf("%d\n",sizeof(Arr));    //Size
+      printf("%d\n",Arr[0]); 
+      printf("%d\n",Arr[3]); 
+      printf("%d\n",Arr[6]); 
+
+      printf("%d\n",Arr[2]); 
+      printf("%d\n",Arr[4]); 
+
+    
+
+    return 0 ;
+
+}
