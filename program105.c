ac6ed48cec5832df3d016907344126ebea9377c7
ritesh_gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-07 21:51:45 +0530

    Add files via upload

---
+++ program105.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void Display(int *iPtr)
+{
+    printf("%d\n",*iPtr);
+
+
+}
+
+int main()
+{
+    int Arr[5]= {10,20,30,40,50};
+
+
+    printf("Base addresss of:%d\n",Arr);
+
+    Display(Arr);
+
+
+
+
+
+    return 0 ;
+
+}
