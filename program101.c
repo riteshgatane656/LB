eec97fc1855c8fed444e480f760d650db51263b3
ritesh_gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-07 21:47:58 +0530

    Add files via upload

---
+++ program101.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int iCnt = 0 ;
+    int Arr[5]= {0};
+
+    printf("Enter the Elements:\n");
+   for(iCnt = 0 ;iCnt < 5; iCnt++)
+    { 
+          scanf("%d",&Arr[iCnt]);
+    }
+   
+     printf("Elements of Array are :\n");
+
+     for(iCnt = 0 ;iCnt < 5; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+
+
+    return 0 ;
+
+}
---
+++ program102.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void CallByValue(int iNo)
+{
+    iNo++ ;
+
+    
+}
+
+int main()
+{
+    int iValue =11;
+    CallByValue(iValue);
+
+    printf("Value after Function call:%d\n",iValue);
+
+
+    return 0 ;
+
+}
